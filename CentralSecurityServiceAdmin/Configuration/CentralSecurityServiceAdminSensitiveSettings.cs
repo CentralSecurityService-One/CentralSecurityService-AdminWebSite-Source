a640dcff4c5cd5d8232beacaadf3e0e6cdf9dbed
Eamonn Duffy <Eamonn.Duffy@GMail.com>
2025-07-02 22:33:48 +0100

    feat: Addition of Sensitive E-Mail Settings.

--- CentralSecurityServiceAdmin/Configuration/CentralSecurityServiceAdminSensitiveSettings.cs
+++ CentralSecurityServiceAdmin/Configuration/CentralSecurityServiceAdminSensitiveSettings.cs
@@ -22,6 +22,23 @@
             public string AdminMobilePhoneNumber { get; set; }
         }
 
+        public class EMailSettings
+        {
+            public string HostName { get; set; }
+
+            public int HostPort { get; set; }
+
+            public string SenderEMailAddress { get; set; }
+
+            public string SenderPassword { get; set; }
+
+            public string FromEMailName { get; set; }
+
+            public string FromEMailAddress { get; set; }
+        }
+
         public AdminAccountSettings AdminAccount { get; set; }
+
+        public EMailSettings EMail { get; set; }
     }
 }
